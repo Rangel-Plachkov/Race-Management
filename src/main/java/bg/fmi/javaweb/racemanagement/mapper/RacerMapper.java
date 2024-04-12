feaed565e1e4aa424f612a58f664a0b04d9b19a4
Rangel <rangelplachkov1@gmail.com>
2024-04-13 00:26:26 +0300

    mapping methods for racer

--- src/main/java/bg/fmi/javaweb/racemanagement/mapper/RacerMapper.java
+++ src/main/java/bg/fmi/javaweb/racemanagement/mapper/RacerMapper.java
@@ -1,5 +1,22 @@
 package bg.fmi.javaweb.racemanagement.mapper;
 
+import bg.fmi.javaweb.racemanagement.models.Racer;
+import bg.fmi.javaweb.racemanagement.models.RacerDTO;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
 public interface RacerMapper {
+
+    @Mapping(source = "firstName", target = "firstName")
+    @Mapping(source = "lastName", target = "lastName")
+    @Mapping(source = "age", target = "age")
+    Racer dtoToEntity(RacerDTO racerDTO);
+
+    @Mapping(source = "firstName", target = "firstName")
+    @Mapping(source = "lastName", target = "lastName")
+    @Mapping(source = "age", target = "age")
+    RacerDTO entityToDto(Racer racer);
 
 }
