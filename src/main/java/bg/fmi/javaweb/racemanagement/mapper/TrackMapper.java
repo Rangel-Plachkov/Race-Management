268a10c5a93a0e29ca063245fd38e6661accbbaa
Rangel <rangelplachkov1@gmail.com>
2024-04-13 11:52:12 +0300

    add team controller

---
+++ src/main/java/bg/fmi/javaweb/racemanagement/mapper/TrackMapper.java
@@ -0,0 +1,19 @@
+package bg.fmi.javaweb.racemanagement.mapper;
+
+import bg.fmi.javaweb.racemanagement.models.Track;
+import bg.fmi.javaweb.racemanagement.models.TrackDTO;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface TrackMapper {
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "length", target = "length")
+    Track dtoToEntity(TrackDTO trackDTO);
+
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "length", target = "length")
+    TrackDTO entityToDto(Track track);
+
+}
